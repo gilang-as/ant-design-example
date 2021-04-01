cde51d7f615decbf30c0c5dfcfacc480b9bbc1e0
gilang-as <gilangas@outlook.com>
2021-04-01 14:57:36 +0700

    add docs

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import {
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
 import HomePage from "./pages/home";
+import DashboardPage from "./pages/dashboard";
 
 const App = () => {
   return (
       <Router>
               <Switch>
+                  <Route path="/dashboard">
+                      <DashboardPage />
+                  </Route>
                   <Route path="/auth/sign-up">
                       <RegisterPage />
                   </Route>
